4ee21d70659dd4f2e2bed84b04e2336f968e2067
Vinicius Galassi <viniciusgalassi@gmail.com>
2025-11-27 19:20:03 -0300

    testando github actions (11)

--- backend/tests/integration/createrecipe.test.js
+++ backend/tests/integration/createrecipe.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../../app');
 const sequelize = require('../util/database');
 const Recipe = require('../models/Recipe');
 
