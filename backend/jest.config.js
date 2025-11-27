0c65ee16124f5c6a7bb3eca8511dea22b98d5ae3
Vinicius Galassi <viniciusgalassi@gmail.com>
2025-11-27 18:45:15 -0300

    testando github actions (6)

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: "node",
   verbose: true,
-  testTimeout: 30000,
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"]
+  testTimeout: 30000
 };
--- backend/tests/setup.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require("../models");
-
-beforeAll(async () => {
-  await sequelize.sync({ force: true }); // cria tabelas limpas
-});
-
-afterAll(async () => {
-  await sequelize.close(); // evita “Cannot log after tests are done”
-});
