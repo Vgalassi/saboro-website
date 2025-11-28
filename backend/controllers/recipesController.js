5fa117d530731cea23f76530d41817bded432618
Vinicius Galassi <viniciusgalassi@gmail.com>
2025-11-28 02:42:54 -0300

    testando github actions (34)

--- backend/controllers/recipesController.js
+++ backend/controllers/recipesController.js
@@ -16,6 +16,7 @@ exports.getIndexRecipes = (req, res, next) => {
 
 
 exports.createRecipe = (req, res, next) => {
+  return 0;
   if(!req.session.isLoggedIn){
     return res.status(401).json({ error: "Usuário não autenticado" });
   }
